8daff317d405ccda9e7de63778d833aa928fe7b1
Alexey Kovtunovich <kovtalex@gmail.com>
2019-11-03 13:44:46 +0300

    test was fixed

--- molecule/gce/tests/test_default.py
+++ molecule/gce/tests/test_default.py
@@ -5,7 +5,6 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-
 # check if MongoDB is enabled and running
 def test_mongo_running_and_enabled(host):
     mongo = host.service("mongod")
@@ -13,8 +12,8 @@ def test_mongo_running_and_enabled(host):
     assert mongo.is_enabled
 
 # check if configuration file contains the required line
-def test_config_file(File):
-    config_file = File('/etc/mongod.conf')
+def test_config_file(host):
+    config_file = host.file('/etc/mongod.conf')
     assert config_file.contains('bindIp: 0.0.0.0')
     assert config_file.is_file
 
