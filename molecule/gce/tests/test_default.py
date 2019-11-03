9204c4bb358edf5e8e7d9c16b8a0463b24439ac0
Alexey Kovtunovich <kovtalex@gmail.com>
2019-11-03 15:47:57 +0300

    .yamllint was added

--- molecule/gce/tests/test_default.py
+++ molecule/gce/tests/test_default.py
@@ -11,11 +11,13 @@ def test_mongo_running_and_enabled(host):
     assert mongo.is_running
     assert mongo.is_enabled
 
+
 # check if configuration file contains the required line
 def test_config_file(host):
     config_file = host.file('/etc/mongod.conf')
     assert config_file.contains('bindIp: 0.0.0.0')
     assert config_file.is_file
+
 
 # check if mongo port 27017 is listening
 def test_mongo_port_listening(host):
